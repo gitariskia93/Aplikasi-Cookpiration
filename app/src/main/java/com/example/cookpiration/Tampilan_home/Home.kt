b458585a96d2f4b15680c5c9fa86a1f7899e78d0
Kawaii Desu <gitariskia93@gmail.com>
2023-11-30 21:45:06 +0800

    tambah home

---
+++ app/src/main/java/com/example/cookpiration/Tampilan_home/Home.kt
@@ -0,0 +1,12 @@
+package com.example.cookpiration.Tampilan_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cookpiration.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
